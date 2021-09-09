860406ee5ce6b5d1c492b048798c55a8236bac09
sod2003 <sod2003@gmail.com>
2021-09-09 17:03:24 -0400

    Adding exception test

--- app/src/test/java/com/example/myfirstapp/CalculatorTest2.java
+++ app/src/test/java/com/example/myfirstapp/CalculatorTest2.java
@@ -1,5 +1,7 @@
 package com.example.myfirstapp;
 
+import static org.junit.Assert.assertThrows;
+
 import junit.framework.TestCase;
 
 public class CalculatorTest2 extends TestCase {
@@ -9,5 +11,9 @@ public class CalculatorTest2 extends TestCase {
 
     public void testDiv() {
         assertEquals(2, Calculator.div(20, 10));
+    }
+
+    public void testDiv2() { assertThrows(ArithmeticException.class,
+            () -> { Calculator.div(1, 0);});
     }
 }
